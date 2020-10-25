3151832705330fc5ed67d8957a94589980cd2a82
Chris <hargrove.chris91@gmail.com>
2020-10-25 21:43:47 +0000

    Generated Visual Studio Solution
    Used premake to generate the Visual Studio Solution and projects

---
+++ premake5.lua
@@ -0,0 +1,68 @@
+workspace "Covid-Engine"
+    architecture "x64"
+    configurations { "Debug", "Release"}
+
+OutputDirectory = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architexture}"
+
+-- Covid Engine Project Setup
+project "Covid-Engine"
+    location "Covid-Engine"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. OutputDirectory .. "/%{prj.name}")
+    objdir ("bin-int/" .. OutputDirectory .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs {
+        ""
+    }
+
+    filter "configurations:Debug"
+        defines "DEBUG"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines "NDEBUG"
+        runtime "Release"
+        optimize "on"
+
+project "Quarantine"
+    location "Quarantine"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. OutputDirectory .. "/%{prj.name}")
+    objdir ("bin-int/" .. OutputDirectory .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs {
+        "Covid-Engine/src"
+    }
+
+    links {
+        "Covid-Engine"
+    }
+
+    filter "configurations:Debug"
+        defines "DEBUG"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines "NDEBUG"
+        runtime "Release"
+        optimize "on"
